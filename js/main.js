6a24486875ca89ecc6bd8c7e461b1d250954c7ae
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-03-25 20:23:29 +0200

    Added border

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-function generate() {  
+function generate() {
     let x = Math.floor((Math.random()) * 10);
     document.getElementById('results').innerHTML = x;
 }
--- js/mainqueries.js
+++ js/mainqueries.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     $('.main-button').click(function () {
         let fucking = $('.creator');
-        fucking.animate({width: 'toggle'}, "fast");
-        
+        fucking.animate({ width: 'toggle' }, "fast");
+
     });
 });
 
 
 $(document).ready(function () {
-    $('.main-button-1').click(function(){
+    $('.main-button-1').click(function () {
         $('.show-rules').slideToggle(500);
         $('.show-rules span').css('color', 'red');
         $('.show-rules em').css('color', 'green');
