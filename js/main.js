2b254005c8fee68d0ae7dba4f5d273b5a5fb6530
justaway1 <44018646+justaway1@users.noreply.github.com>
2022-03-31 23:25:59 +0300

    Added the final stuff

--- js/main.js
+++ js/main.js
@@ -1,5 +1,58 @@
+const btnClick = document.getElementById('random-button-1');
+const lives = document.getElementById('showLives');
+const winner = document.getElementById('winningNumbers');
+// The Event Listeners
+btnClick.addEventListener('click', generateNumber);
+btnClick.addEventListener('click', generateLives);
+// btnClick.addEventListener('click', createButton);
 
-function generate() {
-    let x = Math.floor((Math.random()) * 10);
-    document.getElementById('results').innerHTML = x;
+
+// function createButton(){
+//     let button = document.createElement('input');
+//     button.type = 'button';
+//     button.value = 'Try Again'
+//     button.classList.add('reseter');
+//     button.onclick = reloadPage;
+
+// }
+
+function reloadPage() {
+    window.location.reload();
 }
+
+// Function for the random Number
+function generateNumber() {
+    // let tryAgain = createButton();
+    let randomNumber = Math.floor(Math.random() * 21);
+    document.getElementById('results').innerHTML = randomNumber;
+    if (randomNumber === 5 || randomNumber === 15) {
+        winner.innerHTML = `Congratulation!<br/>You won with ${randomNumber}`;
+        btnClick.disabled = true;
+        btnClick.style.opacity = 0.5;
+        results.style.display = 'none';
+        // document.getElementById('tryAgain').innerHTML = tryAgain;
+    }
+}
+
+// Function for the showing lives
+let live = 6;
+function generateLives() {
+    live--;
+    lives.innerHTML = `${live} LIVES !`;
+    lives.style.background = 'limegreen';
+    lives.style.color = 'white';
+
+    if (live === 1) {
+        lives.innerHTML = `${live} LIFE !!! `;
+    }
+
+    if (live === 0) {
+        lives.innerHTML = `Game Over`;
+        btnClick.disabled = true;
+        lives.style.color = 'white';
+        lives.style.background = 'red'
+        btnClick.style.opacity = 0.5;
+        document.getElementById('results').style.display = 'none';
+    }
+
+};
